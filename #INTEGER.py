d44a0652e839e19aac12a05841587c9ac3916141
choco302 <hazeldeguiasimorlan@gmail.com>
2024-11-26 14:19:34 +0800

    Add files via upload

---
+++ #INTEGER.py
@@ -0,0 +1,28 @@
+#INTEGER
+k = int(3)
+l = int(6)
+m = int(2)
+print (k)
+print (l)
+print (m)
+
+
+
+#FLOAT
+a = float (5)
+b = float (4)
+c = float (2.3)
+d = float (4.9)
+print (a)
+print (b)
+print (c)
+print (d)
+
+
+#STRING
+x = str("s24")
+y = str(8)
+z = str("hello universe")
+print(x)
+print(y)
+print(z)
