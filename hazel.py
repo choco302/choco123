23c5beb5b91cacdcf91610bea9323b6b49c5d160
choco302 <hazeldeguiasimorlan@gmail.com>
2024-11-26 15:03:35 +0800

    Update hazel.py
    correction

--- hazel.py
+++ hazel.py
@@ -24,7 +24,7 @@ st.write("""
 
 # Section: Interests
 st.header("My Interests 💡")
-I am passionate about several topics and activities, to do:
+I am passionate about several topics and activities:
 - **[Interest 1]**: (e.g., Web Development, Machine Learning, Software Engineering)
 
 
